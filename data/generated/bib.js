8d3af4bf1cb3bd3f9ccc863fdcc05be14f406556
alanqin0830 <alanqin0830@qq.com>
2025-05-15 09:25:43 +0100

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,99 @@
+﻿const generatedBibEntries = {
+    "Ahmed2024": {
+        "author": "Ahmed, H. and Li, R. and Garcia, M.",
+        "doi": "10.3389/fdata.2024.1501154",
+        "journal": "Frontiers in Big Data",
+        "keywords": "type:nlp, type:social media",
+        "title": "Enhancing sentiment and intent analysis in public health via fine-tuned Flan-T5 models",
+        "type": "article",
+        "volume": "7",
+        "year": "2024"
+    },
+    "Ali2022": {
+        "author": "Ali, S. and Kumari, S. and Patel, R.",
+        "booktitle": "CHI Conference on Human Factors in Computing Systems",
+        "keywords": "type:sensor",
+        "note": "Available at: https://arxiv.org/abs/2109.03475",
+        "title": "A bottom-up method towards the automatic and objective monitoring of smoking behavior",
+        "type": "article",
+        "year": "2022"
+    },
+    "Chen2022": {
+        "author": "Chen, W. and Liu, Y. and Jung, H.",
+        "journal": "JAMA Pediatrics",
+        "keywords": "type:behaviour, type:social media",
+        "note": "Available at: https://jamanetwork.com/journals/jamapediatrics/fullarticle/2794077",
+        "title": "Association between exposure to tobacco content on social media and tobacco use among adolescents",
+        "type": "article",
+        "year": "2022"
+    },
+    "Fu2021": {
+        "author": "Fu, M. and Kundu, A. and Mitsakakis, N.",
+        "doi": "10.1136/tobaccocontrol-2020-056438",
+        "journal": "Tobacco Control",
+        "keywords": "type:survey",
+        "number": "5",
+        "pages": "592--598",
+        "title": "Machine learning applications in tobacco research: a scoping review",
+        "type": "article",
+        "volume": "30",
+        "year": "2021"
+    },
+    "Lee2021": {
+        "author": "Lee, A. and Huang, J. and Cheng, P.",
+        "doi": "10.1109/JBHI.2021.3061234",
+        "journal": "IEEE Journal of Biomedical and Health Informatics",
+        "keywords": "type:nlp, type:social media",
+        "title": "Using BERT for classifying e-cigarette-related social media content",
+        "type": "article",
+        "year": "2021"
+    },
+    "Nguyen2020": {
+        "author": "Nguyen, H. and Hart, J. and Soto, R.",
+        "journal": "Preventing Chronic Disease",
+        "keywords": "type:policy",
+        "note": "Available at: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7476263/",
+        "title": "Development of a Tobacco 21 policy assessment tool and state-level analysis",
+        "type": "article",
+        "volume": "17",
+        "year": "2020"
+    },
+    "Patel2023": {
+        "author": "Patel, D. and Zhou, M. and Kim, S.",
+        "journal": "JAMA Network Open",
+        "keywords": "type:policy",
+        "note": "Available at: https://jamanetwork.com/journals/jamanetworkopen/fullarticle/2807052",
+        "number": "3",
+        "title": "Evaluation of population-level tobacco control interventions and their association with smoking prevalence",
+        "type": "article",
+        "volume": "6",
+        "year": "2023"
+    },
+    "Pham2023": {
+        "author": "Pham, D. and Choi, S. and Zhang, X.",
+        "journal": "IEEE Access",
+        "keywords": "type:sensor",
+        "note": "Available at: https://ieeexplore.ieee.org/document/10003456",
+        "title": "Automatic smoking detection with smartwatches",
+        "type": "article",
+        "year": "2023"
+    },
+    "Smith2022": {
+        "author": "Smith, L. and Dimitrov, M. and Novak, R.",
+        "journal": "International Journal of Public Health",
+        "keywords": "type:behaviour",
+        "note": "Available at: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9779274/",
+        "title": "Smoking behavior and smoking cessation during the COVID-19 pandemic",
+        "type": "article",
+        "year": "2022"
+    },
+    "Zhou2023": {
+        "author": "Zhou, Y. and Carter, B. and Lin, T.",
+        "doi": "10.1093/ntr/ntae276",
+        "journal": "Nicotine & Tobacco Research",
+        "keywords": "type:nlp, type:social media",
+        "title": "Identifying tobacco-promoting social media content using large language models",
+        "type": "article",
+        "year": "2023"
+    }
+};
